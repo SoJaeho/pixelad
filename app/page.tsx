0e7c2f4c68398342f9819680aa1b30ef3be8703a
SoJaeho <sojh615@naver.com>
2024-12-28 11:48:26 +0900

    db 이미지 더미

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ const pixels = new Array(13500).fill(false);
 
 async function Home() {
   const client = await connectDB;
-  const result = await client.db('pixel').collection('data').find();
+  const result = await client.db('pixel').collection('data').find().toArray();
   return (
-    <div style={{ display: 'grid', gridTemplateColumns: `repeat(150, ${PIXEL_SIZE}px)` }}>
+    <div style={{ position: 'relative',display: 'grid', gridTemplateColumns: `repeat(150, ${PIXEL_SIZE}px)`}}>
       {pixels.map((pixel, index) => (
         <div
           key={index}
@@ -16,6 +16,21 @@ async function Home() {
             backgroundColor: pixel ? 'blue' : 'lightgray', 
             border: '1px solid #ddd',
             cursor: 'pointer',
+          }}
+        />
+      ))}
+      {result.map((data, index) => (
+        <div
+          key={index}
+          style={{
+            position: "absolute",
+            top: data.starty*PIXEL_SIZE,
+            left: data.startx*PIXEL_SIZE,
+            width: (data.endx-data.startx+1)*PIXEL_SIZE,
+            height: (data.endy-data.starty+1)*PIXEL_SIZE,
+            backgroundColor: 'blue',
+            border: '1px solid #ddd',
+            
           }}
         />
       ))}
